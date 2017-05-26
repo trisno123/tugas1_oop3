1fbf9f9dddf30d6a93cfdfd4fba9ddff777557e2
trisno123 <trisnomarsono@yahoo.com>
2017-05-26 14:18:02 +0700

    Update hitungluas.java

--- hitungluas.java
+++ hitungluas.java
@@ -1,7 +1,21 @@
-class hitungluas{
-	public static int cariluaspanjang(int panjang, int luas){
-	return panjang * lebar;
-}
-	public static vold main(String[] args){
-	}	
+import java.util.*;
+
+public class lingkaran
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		double phi = 3.14;
+		double r, luas,keliling;
+
+		System.out.println("Program Luas Lingkaran\n");
+		System.out.print("Masukkan Panjang Jari-jari : ");
+		r = input.nextDouble();
+
+		luas = 0.5 * phi * r * r;
+		keliling = 2 * phi * r;
+		
+		System.out.print("Luas Lingkaran = " + (int)luas + " \nKeliling lingkaran = "+ (int)keliling);
+	}
 }
